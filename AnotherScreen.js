5f473d1f88f08f2416d1dd308dd9c7b50373004f
ilies ouldmenouer <63064471+ilyasxdz@users.noreply.github.com>
2020-08-07 16:51:47 +0100

    Seting up Provider
    creatign the store - reducer - dispatcher and the provider

---
+++ AnotherScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export const AnotherScreen = () => {
+  return (
+    <View>
+      <Text>Hello </Text>
+      <Text></Text>
+    </View>
+  );
+};
--- App.js
+++ App.js
@@ -1,12 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+//Screen :
+import { AnotherScreen } from "./AnotherScreen";
+///------------
 export default function App() {
+  //consts goes here :
+  const initialState = {
+    name: "ilies",
+  };
+  //---
+  //fucntions goes here :
+
+  const reducer = (state = initialState) => {};
+  const store = createStore(reducer);
+
+  //----------
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Provider store={store}>
+        <AnotherScreen />
+      </Provider>
     </View>
   );
 }
@@ -14,8 +30,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
