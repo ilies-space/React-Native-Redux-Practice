70a5b9b98bae8989ca4e91d231add736dc6b61e2
ilies ouldmenouer <63064471+ilyasxdz@users.noreply.github.com>
2020-08-09 11:44:49 +0100

    using State on index.js

--- App/index.js
+++ App/index.js
@@ -1,24 +1,50 @@
 import React, { useState } from "react";
 import { Text, View, Button } from "react-native";
 import Allitems from "./products";
+
+// redux :
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 ///------------
-
-const state = {
-  name: "ilies",
-};
-
-const changeValue = () => {
-  console.log(state.name);
-  setState({ name: "oldm" });
-};
 
 export default () => {
   const [name, setName] = useState("ilies");
+  const [password, setpassword] = useState("PASWORD");
+
+  const data = { name, password };
+
+  //redux :
+  //fucntions goes here :
+
+  const reducer = (state = data, action) => {
+    switch (action.type) {
+      case "update_Value":
+        return { name: "new value" };
+
+      case "deleteValue":
+        return { name: "" };
+
+      case "chnageName":
+        setName(action.value);
+        console.log(action.value);
+        break;
+      case "test":
+        console.log("test");
+      default:
+        break;
+    }
+
+    return state;
+  };
+  const store = createStore(reducer);
+
+  //-------
 
   return (
-    <View style={{ flex: 1, paddingTop: 36 }}>
-      <Text> {name} </Text>
-      <Button title="change to Oldm" onPress={() => setName("oldm")} />
+    <View style={{ flex: 1 }}>
+      <Provider store={store}>
+        <Allitems />
+      </Provider>
     </View>
   );
 };
--- App/products.js
+++ App/products.js
@@ -1,12 +1,33 @@
 import React, { Component } from "react";
 import { Text, View, Button } from "react-native";
+import { connect } from "react-redux";
 
-export default class Allitems extends Component {
+class Allitems extends Component {
   render() {
     return (
-      <View style={{ flex: 1, paddingTop: 36 }}>
-        <Text>2</Text>
+      <View style={{ flex: 1, paddingTop: 36, alignItems: "center" }}>
+        <Text> {this.props.name} </Text>
+        <Button
+          title="change to Oldm"
+          onPress={() => this.props.chnageName("Ouldenoeur")}
+        />
+        <Button title="test" onPress={() => this.props.test()} />
       </View>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    name: state.name,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    test: () => dispatch({ type: "test" }),
+    chnageName: (value) => dispatch({ type: "chnageName", value }),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Allitems);
