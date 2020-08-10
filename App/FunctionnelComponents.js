69372d8ceefd3d359c7e07bebf0a47228fdebd88
ilies ouldmenouer <63064471+ilyasxdz@users.noreply.github.com>
2020-08-10 08:29:30 +0100

    Displaying list from reducer data

--- App/FunctionnelComponents.js
+++ App/FunctionnelComponents.js
@@ -3,10 +3,18 @@ import { View, Text } from "react-native";
 import { useSelector } from "react-redux";
 
 export const FunctionnelComponents = () => {
-  const name = useSelector((state) => state.name);
+  const AllProducts = useSelector((state) => state.AllProducts);
+
   return (
     <View style={{ alignItems: "center", justifyContent: "center", flex: 1 }}>
-      <Text> {name} </Text>
+      {AllProducts.map((data, key) => {
+        return (
+          <View style={{ flexDirection: "row", alignContent: "flex-start" }}>
+            <Text> {data.ProductName} </Text>
+            <Text> {data.ProductPrice} </Text>
+          </View>
+        );
+      })}
     </View>
   );
 };
